0821e77fbeb246d5c9effc38eac35c60a8c00bfa
Stefany Yason <48705140+stefanyyason@users.noreply.github.com>
2019-11-30 12:00:38 +0800

    Add files via upload

---
+++ Filter.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.awt.event.KeyEvent;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author ASUS
+ */
+public class Filter {
+    public static void digitFilter(KeyEvent evt){
+        char input = evt.getKeyChar();
+        if(!((Character.isDigit(input)) ||
+                (input == KeyEvent.VK_BACK_SPACE) ||
+                (input == KeyEvent.VK_DELETE))){
+            
+            new JFrame().getToolkit().beep();
+            JOptionPane.showMessageDialog(null, "error");
+            
+        }
+    }
+}
